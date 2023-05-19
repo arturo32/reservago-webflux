a161920c56fc836f112d4eecbcda213774af552e
Arturo Fonseca <arturofonsecas32@gmail.com>
2023-05-18 21:03:42 -0300

    fix: acess to other services from checkout service

--- checkout/src/main/java/br/ufrn/imd/reservagowebflux/checkout/CheckoutApplication.java
+++ checkout/src/main/java/br/ufrn/imd/reservagowebflux/checkout/CheckoutApplication.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Checkout API REST", version = "1.0", description = "API documentation of Checkout service"))
@@ -11,6 +14,12 @@ public class CheckoutApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckoutApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebClientBuilder() {
+		return WebClient.builder();
 	}
 
 }
